0fe9eeaaa27cfa07452f86a9b11c94b5907c462e
Captressketh001 <shininglite2012@gmail.com>
2023-09-20 14:32:09 +0100

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import { express } from "express";
-import router from "./routes";
+import indexRoute from "./routes/index";
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json({'limit': '5mb'}));
-app.use(router);
-app.listen(port);
+app.use(indexRoute);
+app.listen(PORT);
